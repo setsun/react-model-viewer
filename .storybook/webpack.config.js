008d086950a70f399b0a48ed2947c5d1a7227163
setsun <setsun.xyz@proton.me>
2019-05-19 22:15:49 -0400

    feat(init): init

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+// you can use this file to add your custom webpack plugins, loaders and anything you like.
+// This is just the basic way to add additional webpack configurations.
+// For more information refer the docs: https://storybook.js.org/configurations/custom-webpack-config
+
+// IMPORTANT
+// When you add this file, we won't add the default configurations which is similar
+// to "React Create App". This only has babel loader to load JavaScript.
+
+module.exports = {
+  resolve: {
+    modules: [path.resolve('./node_modules'), path.resolve('./src')],
+    extensions: ['*', '.js', '.jsx', '.ts', '.tsx', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [{ loader: 'babel-loader' }],
+      },
+      {
+        test: /\.stories\.tsx?$/,
+        use: [{ loader: require.resolve('@storybook/addon-storysource/loader') }],
+        enforce: 'pre',
+      },
+      {
+        test: /\.js(x?)$/,
+        exclude: /(node_modules)/,
+        use: [{ loader: 'babel-loader' }],
+      },
+    ],
+  },
+  plugins: [
+    // your custom plugins
+  ],
+};
